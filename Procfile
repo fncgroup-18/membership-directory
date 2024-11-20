f22d44669ff6c7eaac318138f61a9ab3d75c079f
fncgroup-18 <fncgroup-18@github.com>
2024-11-20 12:20:22 +0000

    Updated deployment configuration with wsgi.py

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn "app:create_app()" --log-file -
+web: gunicorn wsgi:app --log-file -
--- run.py
+++ run.py
@@ -2,7 +2,10 @@ from app import create_app, db
 
 app = create_app()
 
-if __name__ == '__main__':
+def init_db():
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    init_db()
+    app.run()
